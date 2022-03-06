cfce13835ea55b45c7064916daba38d8b2b4b8de
Akshay <akshay.ap95@gmail.com>
2022-03-06 23:39:23 +0100

    Feature: Create ethereum adapter from parameters

--- src/main/java/blockchains/iaas/uni/stuttgart/de/plugin/ethereum/EthereumPlugin.java
+++ src/main/java/blockchains/iaas/uni/stuttgart/de/plugin/ethereum/EthereumPlugin.java
@@ -13,10 +13,13 @@ package blockchains.iaas.uni.stuttgart.de.plugin.ethereum;
 
 import blockchains.iaas.uni.stuttgart.de.api.IAdapterExtension;
 import blockchains.iaas.uni.stuttgart.de.api.interfaces.BlockchainAdapter;
+import blockchains.iaas.uni.stuttgart.de.api.utils.PoWConfidenceCalculator;
 import org.pf4j.Extension;
 import org.pf4j.Plugin;
 import org.pf4j.PluginWrapper;
+import org.web3j.crypto.CipherException;
 
+import java.io.IOException;
 import java.util.Map;
 
 public class EthereumPlugin extends Plugin {
@@ -40,8 +43,24 @@ public class EthereumPlugin extends Plugin {
         @Override
         public BlockchainAdapter getAdapter(Map<String, Object> parameters) {
             String nodeUrl = (String) parameters.get("nodeUrl");
-            int averageBlockTimeSeconds = (int) parameters.get("averageBlockTimeSeconds");
-            return new EthereumAdapter(nodeUrl, averageBlockTimeSeconds);
+            String keystorePassword = (String) parameters.get("keystorePassword");
+            String keystorePath = (String) parameters.get("keystorePath");
+            double adversaryVotingRatio = (double) parameters.get("adversaryVotingRatio");
+
+            int pollingTimeSeconds = (int) parameters.get("pollingTimeSeconds");
+            final EthereumAdapter adapter = new EthereumAdapter(nodeUrl, pollingTimeSeconds);
+
+            final PoWConfidenceCalculator cCalc = new PoWConfidenceCalculator();
+            cCalc.setAdversaryRatio(adversaryVotingRatio);
+
+            try {
+                adapter.setCredentials(keystorePassword, keystorePath);
+            } catch (IOException | CipherException e) {
+                e.printStackTrace();
+            }
+
+            adapter.setConfidenceCalculator(cCalc);
+            return adapter;
         }
 
         @Override
